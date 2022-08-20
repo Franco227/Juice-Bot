5b9411d3b0fe723be498159cf15ab7d588d2443d
Franco227 <227franco@gmail.com>
2022-08-20 22:56:48 +0200

    ✨ Added logging of deleted messages, added random status changing every 10 mins, added FAQ length check, improved readability

--- JuiceBot.py
+++ JuiceBot.py
@@ -12,17 +12,26 @@ bot = commands.Bot(command_prefix = commands.when_mentioned_or(prefix), help_com
 client_id = "976753770916630528"
 permissions = 8
 invite_link = f"https://discord.com/oauth2/authorize?client_id={client_id}&scope=bot&permissions={permissions}"
-bot_version = "0.11"
+bot_version = "0.12"
+
+
+
+######################
+####### UTILS ########
+######################
+
+
+dInt = discord.OptionType.integer
+colors = discord.Color
 
 def dtime():
     return datetime.utcnow()
 
 def ping_color(latency):
-    c = discord.Color
-    colours = [16711680, c.red(), c.orange(), c.gold(), c.green(), 65280]
+    c = [16711680, colors.red(), colors.orange(), colors.gold(), colors.green(), 65280]
     values = [5000, 750, 500, 250, 100]
     for i, nbr in enumerate(values):
-        if latency >= nbr: return colours[i]
+        if latency >= nbr: return c[i]
     return green
 
 def get_token():
@@ -35,9 +44,11 @@ def get_random_status():
         return texts[randrange(len(texts))]
 
 
+
 ######################
 ##### CATEGORIES #####
 ######################
+
 
 def categories():
     with open("categories.json", 'r') as cats:
@@ -59,26 +70,31 @@ def get_faq(channel):
 
 def get_faq_embed(channel):
     if channel not in channels():
-        return discord.Embed(title = "Unknown Category", description = "This channel doesn't have an FAQ...", color = discord.Color.red())
+        return discord.Embed(title = "Unknown Category", description = "This channel doesn't have an FAQ...", color = colors.red())
     cat = get_cat(channel)
-    return discord.Embed(title = cat.get("name"), description = cat.get("faq").replace("\\n", "\n"), color = discord.Color.green())
+    return discord.Embed(title = cat.get("name"), description = cat.get("faq").replace("\\n", "\n"), color = colors.green())
 
 def get_cat_index(cat):
     return categories().index(cat)
 
+
+
 ######################
 ##### BOT EVENTS #####
 ######################
+
 
 @bot.event
 async def on_ready():
     t = str(datetime.now())
     time = f"the {t[8:10]}/{t[5:7]}/{t[:4]} at {t[11:13]}h{t[14:16]}"
     print(f" Bot connected {time}")
-    connected = discord.Embed(title = f"Bot connected {time} !", colour = discord.Colour.green())
+    connected = discord.Embed(title = f"Bot connected {time} !", color = colors.green())
     try: await bot.get_guild(log_guild).get_channel(log_channel).send(embed = connected)
     except: pass
-    await bot.change_presence(activity = discord.Game(name = get_random_status()))
+    while 1:
+        await bot.change_presence(activity = discord.Game(name = get_random_status()))
+        await asyncio.sleep(600)
 
 
 @bot.event
@@ -87,6 +103,16 @@ async def on_message(message):
     if message.guild.id != 936167959431364628: return
     msg = message.content.split(' ')
     if "virst" in msg or "virt" in msg: await message.reply("crimson*")
+
+
+@bot.event
+async def on_message_delete(message):
+    if message.guild.id != 936167959431364628 or message.channel.id == 1006553062464299109: return
+    msg = discord.Embed(title = f"Message deleted", description = message.content, color = colors.red(), timestamp = dtime())
+    msg.set_author(name = f"{message.author} ({message.author.id})", icon_url = message.author.avatar.url)
+    msg.add_field(name = "Channel", value = f"<#{message.channel.id}>")
+    await bot.get_channel(1006553062464299109).send(embed = msg)
+
 
 
 ######################
@@ -98,6 +124,8 @@ async def on_message(message):
 async def version(inter):
     await inter.send(f"Version {bot_version}")
 
+
+
 @bot.slash_command(description = "Change the bot's status")
 async def change_status(inter):
     if bot.get_guild(936167959431364628).get_role(936168762078560266) not in inter.author.roles:
@@ -106,24 +134,31 @@ async def change_status(inter):
     await bot.change_presence(activity = discord.Game(name = get_random_status()))
     await inter.send("Done !", ephemeral = True)
 
+
+
 @bot.slash_command(description = "Do it first")
 async def dif(inter):
     await inter.send("Dear runner,\n\nDo it first.\n\n- The Mods")
+
+
+
 
 @bot.slash_command(description = "Display bot latency")
 async def ping(inter):
     timePing = time.monotonic()
     await inter.send(":ping_pong: **Pong !**")
-    botLatency = round(1000 * (time.monotonic() - timePing), 2)
-    if botLatency >= 1000: seconds = f" (`{round(botLatency / 1000)}s`)"
+    latency = round(1000 * (time.monotonic() - timePing), 2)
+    if latency >= 1000: seconds = f" (`{round(latency / 1000)}s`)"
     else: seconds = ""
-    embed = discord.Embed(title = ":ping_pong: **Pong !**", description = f"**:robot: Bot Latency :** `{botLatency}ms`{seconds}", color = ping_color(botLatency))
+    embed = discord.Embed(title = ":ping_pong: **Pong !**", description = f"**:robot: Bot Latency :** `{latency}ms`{seconds}", color = ping_color(latency))
     await inter.edit_original_message(content = "", embed = embed)
+
 
 
 @bot.slash_command(description = "Display the FAQ for the current channel")
 async def faq(inter):
     await inter.send(embed = get_faq_embed(inter.channel_id))
+
 
 
 @bot.slash_command(description = "Update the FAQ of the current channel",
@@ -146,6 +181,7 @@ async def update_faq(inter, text):
     await inter.send("The FAQ for the current channel got updated successfully !")
 
 
+
 @bot.slash_command(description = "Create an FAQ for the current channel", options = [
                     discord.Option(name = "name", description = "The name of the category", required = True),
                     discord.Option(name = "text", description = "The new FAQ", required = True) ])
@@ -155,6 +191,9 @@ async def new_faq(inter, name, text):
         return
     if inter.channel_id in channels():
         await inter.send("This channel already has an FAQ... Maybe you wanted to do /update_faq ?", ephemeral = True)
+        return
+    if len(text) > 2000:
+        await inter.send(f"The faq can't be longer than 2000 characters due to discord limitations.\nCharacters : {len(text)}", ephemeral = True)
         return
     with open("categories.json", 'r') as cts:
         cats = json.load(cts)
@@ -167,6 +206,7 @@ async def new_faq(inter, name, text):
     await inter.send("The FAQ for the current channel has been set successfully!")
 
 
+
 @bot.slash_command(description = "Delete the current channel's FAQ")
 async def delete_faq(inter):
     if bot.get_guild(936167959431364628).get_role(936168762078560266) not in inter.author.roles:
@@ -174,6 +214,9 @@ async def delete_faq(inter):
         return
     if inter.channel_id not in channels():
         await inter.send("This channel doesn't have an FAQ...", ephemeral = True)
+        return
+    if len(text) > 2000:
+        await inter.send(f"The faq can't be longer than 2000 characters due to discord limitations.\nCharacters : {len(text)}", ephemeral = True)
         return
     with open("categories.json", 'r') as cts:
         cats = json.load(cts)
@@ -225,7 +268,7 @@ async def odds(inter, of, nb, nb_trials):
                     discord.Option(name = "channel_id", description = "The ID of the channel of the poll", required = True),
                     discord.Option(name = "message_id", description = "The ID of the message of the poll", required = True),
                     discord.Option(name = "number_of_reactions", description = "Number of reaction, leave empty for a yes/no poll", required = False,
-                                    type = discord.OptionType.integer, min_value = 2, max_value = 10) ])
+                                    type = dInt, min_value = 2, max_value = 10) ])
 async def poll(inter, channel_id, message_id, number_of_reactions = 0):
     reactions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
     if bot.get_guild(936167959431364628).get_role(936168762078560266) not in inter.author.roles:
