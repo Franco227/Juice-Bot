141519b4ba4f95d262c748f8d45535f38464ed75
Franco227 <227franco@gmail.com>
2024-08-14 00:23:27 +0200

    Added command check_runs (seed validity check by sophie)

--- JuiceBot.py
+++ JuiceBot.py
@@ -1,6 +1,8 @@
 import asyncio
-import time
 import json
+import re
+import time
+from aiohttp import ClientSession
 import disnake as discord
 from disnake.ext import commands
 from datetime import datetime, UTC
@@ -61,6 +63,45 @@ def get_random_status():
         return texts[randrange(len(texts))]
 
 
+######################
+### SEED VALIDITY ####
+######################
+
+base_url_start = 'https://www.speedrun.com/api/v2/GetGameLeaderboard2?_r=eyJwYXJhbXMiOnsiZ2FtZUlkIjoibTFtbmpleGQiLCJjYXRlZ29yeUlkIjoi'
+base_url_end = 'iLCJ2ZXJpZmllZCI6MH19'
+request_data_list = [
+    ('emQzcWo3OGs', '4lx75xrq'),  # Fishtank
+    ('bWtleXhnOTI', '5q8w44yq'),  # CNI
+    ('OWQ4bDU3cTI', 'xqky9odl'),  # Break Beenest
+    ('amRybncwbGs', 'jqzzvp4q'),  # All Arthropods
+    ('N2RneTczbGQ', '5q8w26rq'),  # All Buckets
+    ('OWQ4bGxsbDI', '81wxmool'),  # All Deaths
+    ('eGQxNXc2ODI', '013n28dq'),  # Half Death
+    ('cmtsbjh4dzI', '810kw35q'),  # Saddle Master
+    ('emRueWc0N2s', {'z19o7z81', 'p12y324q', '81pm8x81', 'xqky7zkl'}),  # Low%
+    ('dmRveWpneTI', 'rqv5jkw1'),  # Herobrine
+    ('cTI1NWpxeTI', 'jq68w93l'),  # Obtain Advancement
+    ('cmtsMDA5bms', 'gq7yyxyq'),  # Any% Superflat
+    ('eGQxd3pvNDI', '9qj3zkol'),  # Quater
+    ('emRubzVybjI', 'mlnx63oq'),  # Mojang Banner
+    ('emQzNWdtOGs', 'zqo40dx1'),  # SMAC
+    ('d2twcnEzajI', 'p1277n4q'),  # All Nametags
+    ('MDJxN2x3emQ', '21dmj0p1'),  # ACI
+    ('N2tqNzh6eGs', 'jqzm4zkl'),  # All Structures
+    ('dmRvbjVveWs', '814n00jl'),  # Aether
+    ('emQzbXZlbjI', 'lx55xgr1'),  # WR
+    ('d2RtcDdyM2Q', '139x86y1')  # All Sands
+]
+
+seed_regex = r'-?\d{14,}'
+
+def check_seed_validity(seed):
+    seed = seed % 2**64
+    a = 18218081
+    b = 1 << 48
+    c = 7847617
+    d = ((((c * ((24667315 * (seed >> 32) + a * (seed % 2**32) + 67552711) % 2**64 >> 32) - a * (((-4824621 * (seed >> 32) + c * (seed % 2**32) + c) + 2**63) % 2**64 - 2**63 >> 32)) - 11) * 0xdfe05bcb1365) % b)
+    return (((((0x5deece66d * d + 11) % b) >> 16) << 32) + (((((0xbb20b4600a69 * d + 0x40942de6ba) % b) >> 16) + 2**31) % 2**32 - 2**31)) % 2**64 == seed
 
 ######################
 ##### CATEGORIES #####
@@ -491,6 +532,47 @@ async def dm(inter, user, message):
     except Exception as e:
         await inter.send(f"Couldn't send the message... ({e})", ephemeral = True)
 
+
+@bot.slash_command(description = "Check pending runs for invalid seed")
+async def check_runs(inter):
+    if bot.get_guild(GUILD_ID).get_role(MOD_ROLE_ID) not in inter.author.roles:
+        await inter.send("You do not have the permission to do that !", ephemeral = True)
+        return
+    await inter.send("Scanning for invalid seeds...")
+    invalid_seeds = []
+    runs_scanned = 0
+    async with ClientSession() as session:
+        for request_data in request_data_list:
+            async with session.get(base_url_start + request_data[0] + base_url_end) as response:
+                data = await response.json()
+                for run in data['runList']:
+                    runs_scanned += 1
+                    if 'comment' not in run:
+                        continue
+                    if request_data[0] == 'emRueWc0N2s':
+                        if not any(value_id in request_data[1] for value_id in run['valueIds']):
+                            continue
+                    elif request_data[1] not in run['valueIds']:
+                        continue
+                    match = re.search(seed_regex, run['comment'])
+                    if not match:
+                        continue
+                    description_seed = int(match.group())
+                    if not check_seed_validity(description_seed):
+                        invalid_seeds.append((run["id"], description_seed))
+
+
+    description = f"{runs_scanned} runs scanned,\n{len(invalid_seeds)} invalid seed(s) found.\n"
+    for run_id, seed in invalid_seeds:
+        description += f"\n**Run [{run_id}](https://speedrun.com/mc_juice/runs/{run_id}) :** `{seed}`"
+
+    embed = discord.Embed(
+        title="Scan complete!",
+        description=description,
+        timestamp=dtime(),
+        color=colors.green() if len(invalid_seeds) == 0 else colors.red()
+    )
+    await inter.edit_original_message(content = "", embed = embed)
 
 
 bot.run(get_token())
