1eddd38f38e5e1e2a48033c1c41f27aa4d36d315
Franco227 <227franco@gmail.com>
2026-06-30 23:46:49 +0200

    Added spam detection for 2jpg+role_mentions

--- cogs/events.py
+++ cogs/events.py
@@ -57,18 +57,20 @@ class EventsCog(commands.Cog):
                 await message.add_reaction(reaction)
 
         # Spambot prevention
-        if len(message.attachments) == 4 and all(attachment.content_type == "image/jpeg" for attachment in message.attachments):
-            await self.delete_and_remove_access(message, "4jpg")
-            # if all(attachment.filename.startswith("IMG_") for attachment in message.attachments):
-            #     await self.delete_and_remove_access(message, "IMG_")
-            # if all(attachment.filename.split('.')[0] == str(i + 1) for i, attachment in enumerate(message.attachments)):
-            #     await self.delete_and_remove_access(message, "1234")
+        attachments_nb = len(message.attachments)
+        has_mentions = len(message.role_mentions) > 0 or "@everyone" in message.content or "@here" in message.content
+        is_all_jpg = all(attachment.content_type == "image/jpeg" for attachment in message.attachments)
+        if is_all_jpg:
+            if attachments_nb == 4:
+                await self.delete_and_remove_access(message, "4jpg")
+            if attachments_nb == 2 and has_mentions:
+                await self.delete_and_remove_access(message, "2jpg+role_mentions")
         if message.content.count('|') >= 100:
             await self.delete_and_remove_access(message, "spoilers")
         if message.content.count("imgur") == 4:
-            await self.delete_and_remove_access(message, "imgur")
-        if "discord.gg/" in message.content and (len(message.role_mentions) != 0 or "@everyone" in message.content or "@here" in message.content):
-            await self.delete_and_remove_access(message, "role_mentions")
+            await self.delete_and_remove_access(message, "4imgur")
+        if "discord.gg/" in message.content and has_mentions:
+            await self.delete_and_remove_access(message, "dgg+role_mentions")
 
 
     @commands.Cog.listener()
