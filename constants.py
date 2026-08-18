35ddfb85004221ed23060f0af2635889a1a45bfb
Franco227 <227franco@gmail.com>
2026-08-18 18:39:57 +0200

    Moved token and IDs to .env

--- constants.py
+++ constants.py
@@ -1,30 +1,35 @@
-from discord import Intents, Color
-from discord.utils import _ColourFormatter
-from logging import FileHandler, Formatter, getLogger, INFO, StreamHandler
 from datetime import date
+from logging import INFO, FileHandler, Formatter, StreamHandler, getLogger
+from os import environ
 from sys import stdout
+
+from discord import Color, Intents
+from discord.utils import _ColourFormatter
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def _env(name: str) -> str:
+    value = environ.get(name)
+    if not value:
+        raise RuntimeError(f"Missing required environment variable: {name}")
+    return value
 
 
 # Bot Data
-BOT_PREFIX = '/'
+BOT_PREFIX = _env("BOT_PREFIX")
 BOT_INTENTS = Intents.all()
-with open("data/TOKEN.txt", 'r') as file:
-    BOT_TOKEN = file.read()
+BOT_TOKEN = _env("BOT_TOKEN")
 with open ("data/version.txt", 'r') as file:
-    BOT_VERSION =  file.read()
+    BOT_VERSION = file.read().strip()
 
 # Ids
-GUILD_ID = 936167959431364628
-LOG_CHANNEL_ID = 1006553062464299109
-HONEYPOT_CHANNEL_ID = 1521634880415596544
-MOD_ROLE_ID = 936168762078560266
-OWNER_ROLE_ID = 963803874911735869
-SUS_ROLE_ID = 1447310460004204694
-
-# Bot Utils
-CLIENT_ID = 976753770916630528
-PERMISSIONS = 8
-INVITE_LINK = f"https://discord.com/oauth2/authorize?client_id={CLIENT_ID}&scope=bot&permissions={PERMISSIONS}"
+GUILD_ID = int(_env("GUILD_ID"))
+LOG_CHANNEL_ID = int(_env("LOG_CHANNEL_ID"))
+HONEYPOT_CHANNEL_ID = int(_env("HONEYPOT_CHANNEL_ID"))
+MOD_ROLE_ID = int(_env("MOD_ROLE_ID"))
+OWNER_ROLE_ID = int(_env("OWNER_ROLE_ID"))
+SUS_ROLE_ID = int(_env("SUS_ROLE_ID"))
 
 # Logger
 _logger_stream_handler = StreamHandler(stdout)
