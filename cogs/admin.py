6f2d54eb27726057a982d903895d55ff826da85e
Franco227 <227franco@gmail.com>
2026-08-19 19:29:14 +0200

    Improved error handling for admin commands

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,8 +2,8 @@ import discord
 from discord.app_commands import checks, command, describe, guilds
 from discord.ext import commands
 
-from constants import GUILD_ID, OWNER_ROLE_ID
-from utils import error_embed, get_random_status, success_embed
+from constants import GUILD_ID, LOG_CHANNEL_ID, LOGGER, OWNER_ROLE_ID
+from utils import default_embed, error_embed, get_random_status, success_embed
 
 
 async def setup(bot: commands.Bot):
@@ -32,21 +32,21 @@ class AdminCog(commands.Cog):
     async def dm(self, interaction: discord.Interaction, user: discord.Member, message: str):
         try:
             await user.send(message.replace('\\n', '\n'))
-            await interaction.response.send_message(embed=success_embed(title="Message sent!", description=""), ephemeral=True)
-        except Exception as e:
-            await interaction.response.send_message(embed=error_embed(f"Couldn't send the message...\n`{e}`"), ephemeral=True)
+            await interaction.response.send_message(embed=success_embed(title="Message sent!", description=f"Content: ```{message}```"), ephemeral=True)
+        except discord.HTTPException as e:
+            await interaction.response.send_message(embed=error_embed(title="Couldn't send message", description=f"```{e}```"), ephemeral=True)
 
     @command(name="send", description="Send a message")
     @guilds(GUILD_ID)
     @checks.has_role(OWNER_ROLE_ID)
     @describe(channel="The channel you want to send the message to")
     @describe(message="Your message, use \\n for new lines")
-    async def send(self, interaction: discord.Interaction, channel: discord.TextChannel, message: str):
+    async def send(self, interaction: discord.Interaction, channel: discord.TextChannel | discord.Thread, message: str):
         try:
-            await channel.send(message.replace('\\n', '\n'))
-            await interaction.response.send_message(embed=success_embed(title="Message sent!", description=""), ephemeral=True)
-        except Exception as e:
-            await interaction.response.send_message(embed=error_embed(f"Couldn't send the message...\n`{e}`"), ephemeral=True)
+            sent_message = await channel.send(message.replace('\\n', '\n'))
+            await interaction.response.send_message(embed=success_embed(title="Message sent!", description=f"{sent_message.jump_url}"), ephemeral=True)
+        except (discord.Forbidden, discord.HTTPException) as e:
+            await interaction.response.send_message(embed=error_embed(title="Couldn't send message", description=f"```{e}```"), ephemeral=True)
 
     @command(name="clear_messages", description="Clear the last messages")
     @guilds(GUILD_ID)
@@ -54,8 +54,13 @@ class AdminCog(commands.Cog):
     @describe(messages="The amount of messages you want to delete")
     @describe(reason="The reason for deletion")
     async def clear_messages(self, interaction: discord.Interaction, messages: int, reason: str = ""):
+        if type(interaction.channel) is not discord.TextChannel and type(interaction.channel) is not discord.Thread:
+            return await interaction.response.send_message(embed=error_embed("Cannot clear messages in this type of channel."))
         try:
-            await interaction.response.send_message(embed=success_embed(title=f"Deleting {messages} messages...", description=f"Reason: {reason}" if reason else ""), ephemeral=True)
+            await interaction.response.send_message(embed=default_embed(title=f"Deleting {messages} messages...", description=f"Reason: {reason}" if reason else ""), ephemeral=True)
             await interaction.channel.purge(limit=messages, reason=reason)
-        except Exception as e:
-            await interaction.response.send_message(embed=error_embed(f"Couldn't delete messages...\n`{e}`"), ephemeral=True)
+            LOGGER.info(f"{messages} purged in {interaction.channel.name} ({interaction.channel_id}) by {interaction.user.name} ({interaction.user.id}).")
+            await interaction.edit_original_response(embed=success_embed(title=f"Deleted {messages} messages.", description=f"Reason: {reason}" if reason else ""))
+            await self.bot.get_channel(LOG_CHANNEL_ID).send(embed=success_embed(title=f"{messages} purged", description=f"{messages} were purged in {interaction.channel.mention} by {interaction.user.mention}."))
+        except (discord.Forbidden, discord.HTTPException) as e:
+            await interaction.followup.send(embed=error_embed(title="Couldn't delete messages", description=f"```{e}```"))
